9c224e31b192906daa38d097bec50159bf9f0e40
hyun <220284hk@gmail.com>
2021-03-07 11:07:26 +0900

    Implement BUTTON feature
    button displays toast

--- app/src/main/java/com/hyunkwak/customviewtest/MainActivity.java
+++ app/src/main/java/com/hyunkwak/customviewtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hyunkwak.customviewtest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Toast.makeText(this, "Hello, I have been clicked!", Toast.LENGTH_LONG).show();
+        });
     }
 }
