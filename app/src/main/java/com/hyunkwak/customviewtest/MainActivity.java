706cef09a2788eb46c2803963843a0bfada9b4eb
hyun <220284hk@gmail.com>
2021-03-07 20:07:37 +0900

    Implement [2/2] NotSpindleToggleButton
    Implemented the logic for the button

--- app/src/main/java/com/hyunkwak/customviewtest/MainActivity.java
+++ app/src/main/java/com/hyunkwak/customviewtest/MainActivity.java
@@ -1,17 +1,52 @@
 package com.hyunkwak.customviewtest;
 
 import android.os.Bundle;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.et);
+        bt = findViewById(R.id.bt_two);
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty())
+                    setOff(bt);
+                else
+                    setOn(bt);
+            }
+        });
+
+    }
+
+    private void setOff(Button bt) {
+        bt.setClickable(false);
+        bt.setBackgroundColor(getResources().getColor(R.color.grey));
+    }
+
+    private void setOn(Button bt) {
+        bt.setClickable(true);
+        bt.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
     }
 }
--- app/src/main/java/com/hyunkwak/customviewtest/NotSpindleToggleButton.java
+++ app/src/main/java/com/hyunkwak/customviewtest/NotSpindleToggleButton.java
@@ -1,21 +1,18 @@
 package com.hyunkwak.customviewtest;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.widget.AppCompatEditText;
 
 public class NotSpindleToggleButton extends RelativeLayout {
-    public Button bt;
-    public AppCompatEditText et;
-
-    public NotSpindleToggleButton(Context context) {
-        super(context);
-    }
+    private Button bt;
+    private AppCompatEditText et;
 
     public NotSpindleToggleButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,7 +21,35 @@ public class NotSpindleToggleButton extends RelativeLayout {
         et = findViewById(R.id.input_edit_text);
         bt = findViewById(R.id.input_button);
 
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty())
+                    setOff(bt);
+                else
+                    setOn(bt);
+            }
+        });
 
     }
+
+    private void setOff(Button bt) {
+        bt.setClickable(false);
+        bt.setBackgroundColor(getResources().getColor(R.color.grey));
+    }
+
+    private void setOn(Button bt) {
+        bt.setClickable(true);
+        bt.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+    }
+
 
 }
